195a40cc4f3b337908362fe1e2adf09ffe2e4c26
AdemMENHOUR <150479331+ademmenh@users.noreply.github.com>
2024-12-14 10:32:22 +0100

    [fix]: fixed error middlewares

--- src/middlewares/error.ts
+++ src/middlewares/error.ts
@@ -1,12 +1,12 @@
 
 import { httpLogs } from "@Types/logs/httpLogs"
 
-import { Request, Response } from 'express'
+import { Request, Response, NextFunction } from 'express'
 
 import { errorResponse } from "@utils/response"
 
 
-export const errorMiddleWare = ((err: Error, req: Request, res: Response) => {
+export const errorMiddleWare = ((err: Error, req: Request, res: Response, next: NextFunction) => {
     return errorResponse (
                 res,
                 httpLogs.InternalServerError.code,
